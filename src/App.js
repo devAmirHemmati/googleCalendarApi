ff1bec6c62e7e7e6183f40bda41665e19e4c1121
Amirreza Hemmati <59439238+devAmirHemmati@users.noreply.github.com>
2022-09-11 09:31:35 +0430

    develop :)

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "gapi-script";
+import GoogleLogin from "react-google-login";
 
-function App() {
+const clientId =
+  "982357118495-2s0vbkajdt2m462q6k9aott6vjp24op6.apps.googleusercontent.com";
+
+const GoogleLoginComponent = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ marginTop: 50, display: "flex", justifyContent: "center" }}>
+      <GoogleLogin
+        clientId={clientId}
+        onSuccess={(response) => {
+          fetch(
+            "https://www.googleapis.com/calendar/v3/calendars/amir.hemmati@apsaaz.com/events",
+            {
+              headers: {
+                authorization: `Bearer ${
+                  response.accessToken || response.code
+                }`,
+              },
+            }
+          )
+            .then((response) => {
+              return response.json();
+            })
+            .then((response) => {
+              console.log(response);
+            })
+            .catch((error) => {
+              console.log(error);
+            });
+        }}
+        onFailure={(error) => {
+          console.log(error);
+        }}
+        cookiePolicy={"single_host_origin"}
+        isSignedIn
+      />
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="App">
+      <GoogleLoginComponent />
+    </div>
+  );
+};
 
 export default App;
